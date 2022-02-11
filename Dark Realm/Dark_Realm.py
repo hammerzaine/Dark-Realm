377654e18248b601d53e0da61f3d4ebc8a842456
Levi Modl <hammer_69_99_99@hotmail.com>
2022-02-10 19:04:53 -0600

    Removed Button Section for now

--- Dark Realm/Dark_Realm.py
+++ Dark Realm/Dark_Realm.py
@@ -18,21 +18,10 @@ from configuration import *
 pygame.display.set_mode((DISPLAYW, DISPLAYH), pygame.RESIZABLE)
 pygame.display.set_caption('Dark Realm')
 mouse = pygame.mouse.get_pos()
-
+Button(140,30, X/2,Y/2)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if width/2 <= mouse[0] <= width/2 + 140 and height/2 <= mouse[1] <= height/2 + 40:
-                pygame.quit()
-
-        if width/2 <= mouse[0] <= width/2 + 140 and height/2 <= mouse[1] <= height/2 + 40:
-            pygame.draw.rect(screen, GREEN, [width/2, height/2, 140, 40])
-        else:
-            pygame.draw.rect(screen, GREEN, [width/2, height/2, 140, 40])
-
-#screen.blit(Text(QTEXT,GREEN), Res())
-pygame.display.update()
+        
--- Dark Realm/configuration.py
+++ Dark Realm/configuration.py
@@ -15,8 +15,9 @@ from includes import *
 #   Variables   #
 #################
 Version = 'v0.1'
-DISPLAYW = 800
-DISPLAYH = 600
+DISPLAYW = 1024
+DISPLAYH = 768
+BLACK = (1,1,1)
 WHITE = (255,255,255)
 GREEN = (0,255,0)
 BLUE = (0,0,128)
@@ -25,14 +26,22 @@ QTEXT = 'QUIT'
 NTEXT = 'NEW GAME'
 OTEXT = 'OPTIONS'
 screen = pygame.display.set_mode((DISPLAYW,DISPLAYH))
-smallfont = pygame.font.SysFont(FONT, 35)
 width = screen.get_width()
 height = screen.get_height()
+X, Y = screen.get_size()
+BUTTON_RES_W = width/2
+BUTTON_RES_H = height/2
 
-def Text(_text_, color):
-    mtext = smallfont.render(_text_, True, color)
+def Text(_text_, color, fontsize):
+    font = pygame.font.SysFont(FONT, fontsize)
+    text = font.render(_text_, True, color)
+    return text
 
 def Res():
-    return (width/2 + 50, height/2)
+    return (width/2, height/2)
+
+def Button(locationx,locationy, sizex,sizey):
+    pygame.draw.rect(screen, GREEN, pygame.Rect(sizex,sizey,locationx,locationy ))
+    pygame.display.flip()
 
 #class Adventure(action):
